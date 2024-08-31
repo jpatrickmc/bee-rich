010f7c00d9697b0c6864a94b37500d5fbd9e5def
Patrick McDaniel <dfwjpm@outlook.com>
2024-08-31 09:25:44 -0500

    add initial routes

---
+++ app/routes/dashboard.expenses.1.tsx
@@ -0,0 +1,10 @@
+import { H2 } from '~/components/headings';
+
+export default function Component() {
+  return (
+    <div className="w-full h-full p-8">
+      <H2>Food</H2>
+      <p>$100</p>
+    </div>
+  );
+}
